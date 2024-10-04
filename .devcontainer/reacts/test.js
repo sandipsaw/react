c8eb61ffa87a16b7f5432a8a8e4d544be57dc790
sandipsaw <sandipsaw449@gmail.com>
2024-10-04 16:21:24 +0000

    container config done

---
+++ .devcontainer/reacts/test.js
@@ -0,0 +1,12 @@
+const accountID = 45126
+let accountEmail = "sandipsaw449@gmail.com" 
+var accountPassword = "12345"
+accountCity="jaipur"
+
+//accountID =  78945
+// console.log(accountID);
+accountEmail = "sandipsaw5212@gmail.com";
+accountPassword = "54321";
+accountCity = "chirkunda";
+//console.log([accountEmail , accountID , accountPassword , accountCity]);
+console.table([accountEmail , accountID , accountPassword , accountCity]);
